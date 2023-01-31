5aa13036b5f9a025210aae84aeffc30f0badea68
z-rayc <zilerayser@gmail.com>
2023-01-31 14:13:01 +0100

    Remove \n in print

--- src/main/java/no/ntnu/idata2305/computation/AsyncSearchSimulator.java
+++ src/main/java/no/ntnu/idata2305/computation/AsyncSearchSimulator.java
@@ -17,7 +17,7 @@ public class AsyncSearchSimulator implements Runnable {
     System.out.println("Request processing started at: " + time1);
     Thread.sleep(10 * 1000);
     long time2 = System.currentTimeMillis();
-    System.out.println("Request processing ended at: " + time2 + "\n");
+    System.out.println("Request processing ended at: " + time2);
   }
   
   public void run() {
