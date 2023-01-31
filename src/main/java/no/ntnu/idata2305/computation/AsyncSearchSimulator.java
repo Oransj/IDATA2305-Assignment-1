9cdcdab2cf743b78979accbecddadcfbd6e4874e
z-rayc <zilerayser@gmail.com>
2023-01-31 14:01:56 +0100

    Create AsyncSearchSimulator.java

---
+++ src/main/java/no/ntnu/idata2305/computation/AsyncSearchSimulator.java
@@ -0,0 +1,30 @@
+package no.ntnu.idata2305.computation;
+
+import java.net.Socket;
+
+public class AsyncSearchSimulator implements Runnable {
+  
+  protected Socket clientSocket = null;
+  protected String serverText = null;
+  
+  public AsyncSearchSimulator(Socket clientSocket, String serverText) {
+    this.clientSocket = clientSocket;
+    this.serverText = serverText;
+  }
+  
+  public void processClientRequest() throws InterruptedException {
+    long time1 = System.currentTimeMillis();
+    System.out.println("Request processing started at: " + time1);
+    Thread.sleep(10 * 1000);
+    long time2 = System.currentTimeMillis();
+    System.out.println("Request processing ended at: " + time2 + "\n");
+  }
+  
+  public void run() {
+    try {
+      processClientRequest();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
